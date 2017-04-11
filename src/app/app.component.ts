c7a3055702538b6823ade0221945df8e61a7da1d
Richard Honor <me@r-honor.uk>
2017-04-11 13:52:49 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./assets/style/main.scss']
+})
+export class AppComponent {
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot(rootRouterConfig)
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {
+
+}
