839d9994a230bf84d8a54cb6884542d1c545dde6
RMHonor <me@r-honor.uk>
2017-10-14 12:51:29 +0100

    Basic shell for skills page

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -6,4 +6,63 @@ import {Component} from '@angular/core';
   templateUrl: './skills.component.html'
 })
 export class SkillsComponent {
+    title: string = 'Skills';
+    technologyGroups = [
+        {
+            header: 'JavaScript',
+            subHeader: 'Experience in a number of modern JavaScript frameworks, with a primary focus on single page application',
+            technologies: [
+                {
+                    name: 'ES6+',
+                    image: '',
+                },
+                {
+                    name: 'Angular 2+',
+                    image: '',
+                },
+                {
+                    name: 'AngularJS',
+                    image: '',
+                },
+                {
+                    name: 'React/Redux',
+                    image: '',
+                },
+                {
+                    name: 'NodeJS',
+                    image: '',
+                },
+                {
+                    name: 'VueJS',
+                    image: '',
+                },
+                {
+                    name: 'D3.js',
+                    image: '',
+                },
+            ],
+        },
+        {
+            header: 'Tooling',
+            subHeader: 'Worked on projects using a number of development tools, considering the performance of the application and benefit to the developers',
+            technologies: [
+                {
+                    name: 'Webpack',
+                    image: '',
+                },
+                {
+                    name: 'NPM',
+                    image: '',
+                },
+                {
+                    name: 'Yarn',
+                    image: '',
+                },
+                {
+                    name: 'Gulp',
+                    image: '',
+                },
+            ],
+        },
+    ];
 }
