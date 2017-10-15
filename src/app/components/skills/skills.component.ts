b171c943d864c6644b8d328f2d822689f1b1bde7
RMHonor <me@r-honor.uk>
2017-10-15 21:55:47 +0100

    Skills component created

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -10,57 +10,85 @@ export class SkillsComponent {
     technologyGroups = [
         {
             header: 'JavaScript',
-            subHeader: 'Experience in a number of modern JavaScript frameworks, with a primary focus on single page application',
+            subHeader: `Experience in a number of modern JavaScript frameworks, with a primary 
+                focus on single page application frameworks. Very deep knowledge of vanilla JS,
+                through reading and online research, notably the You Don't Know JS series.`,
             technologies: [
                 {
                     name: 'ES6+',
-                    image: '',
+                    image: 'es6',
                 },
                 {
                     name: 'Angular 2+',
-                    image: '',
+                    image: 'ng',
                 },
                 {
                     name: 'AngularJS',
-                    image: '',
+                    image: 'ngjs',
                 },
                 {
-                    name: 'React/Redux',
-                    image: '',
+                    name: 'React',
+                    image: 'react',
+                },
+                {
+                    name: 'Redux',
+                    image: 'redux',
                 },
                 {
                     name: 'NodeJS',
-                    image: '',
+                    image: 'node',
                 },
                 {
                     name: 'VueJS',
-                    image: '',
+                    image: 'vue',
                 },
                 {
                     name: 'D3.js',
-                    image: '',
+                    image: 'd3',
                 },
             ],
         },
         {
             header: 'Tooling',
-            subHeader: 'Worked on projects using a number of development tools, considering the performance of the application and benefit to the developers',
+            subHeader: 'Worked on projects using a number of development tools, considering the performance of the application and benefit to the developers.',
             technologies: [
                 {
                     name: 'Webpack',
-                    image: '',
+                    image: 'webpack',
                 },
                 {
                     name: 'NPM',
-                    image: '',
+                    image: 'npm',
                 },
                 {
                     name: 'Yarn',
-                    image: '',
+                    image: 'yarn',
                 },
                 {
                     name: 'Gulp',
-                    image: '',
+                    image: 'gulp',
+                },
+            ],
+        },
+        {
+            header: 'Other',
+            subHeader: 'Knowledge of a whole series of other technologies or methodologies, including some of the below.',
+            technologies: [
+                {
+                    name: 'SASS/SCSS',
+                    image: 'sass',
+                },
+                {
+                    name: 'Spritesheeting',
+                    image: 'spritesheet',
+                },
+                {
+                    name: 'MongoDB',
+                    image: 'mongo',
+                },
+                {
+                    name: 'PHP',
+                    image: 'php'
                 },
             ],
         },
